9c44f8da6000a60d6173df6ae54e97d5ae1032f4
Damian Kowalski <dkowalski.1997@gmail.com>
2026-04-03 08:13:39 +0200

    wip

--- examples/walker.js
+++ examples/walker.js
@@ -1,19 +1,34 @@
 import { createHelper } from "@javascripthub/canvas-helper";
 
 export default function walker(wrapper) {
-  const { setScene, model, destroy } = createHelper(wrapper, {
-    x: 150,
-    y: 150,
-  });
-
-  setInterval(() => {
-    model.x += Math.random() - 0.5;
-    model.y += Math.random() - 0.5;
-  });
+  const walker = new Walker();
+  const { setScene, model, destroy } = createHelper(wrapper, walker);
 
   setScene((draw) => {
+    console.log(draw);
     draw.circle(model.x, model.y, 1);
   });
 
   return destroy;
+}
+
+class Walker {
+  /**
+   * x position of walker
+   */
+  x = 150;
+
+  /**
+   * y position of walker
+   */
+  y = 150;
+
+  constructor() {
+    setInterval(() => this.step());
+  }
+
+  step() {
+    this.x += Math.random() - 0.5;
+    this.y = Math.random() - 0.5;
+  }
 }
