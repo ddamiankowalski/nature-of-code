b7c22e881545adfd31e36f951216318c80ff29d5
Damian Kowalski <dkowalski.1997@gmail.com>
2026-08-23 12:32:46 +0200

    added header text

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,6 +4,6 @@ import { HeaderComponent } from './routes/dashboard/components/header.component'
 @Component({
   selector: 'noc-root',
   imports: [HeaderComponent],
-  template: `<noc-header />`,
+  template: `<noc-header class="flex justify-center w-full" />`,
 })
 export class App {}
--- src/app/routes/dashboard/components/header.component.ts
+++ src/app/routes/dashboard/components/header.component.ts
@@ -2,6 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'noc-header',
-  template: `Witam`,
+  template: ` <header class="flex flex-col justify-center items-center my-10">
+    <h1 class="font-bold text-5xl [word-spacing: -0.75rem]">
+      THE NATURE <br />
+      OF CODE
+    </h1>
+    <h3 class="my-2">IMPLEMENTATION FROM THE BOOK BY DANIEL SHIFFMAN</h3>
+  </header>`,
 })
 export class HeaderComponent {}
