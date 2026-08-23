0a98f17f99f70f76be6b361c22c93d7027bc4b23
Damian Kowalski <dkowalski.1997@gmail.com>
2026-08-23 12:47:58 +0200

    added examples component

--- src/app/routes/examples/examples.component.ts
+++ src/app/routes/examples/examples.component.ts
@@ -1,7 +1,19 @@
 import { Component } from '@angular/core';
 
+type Example = { header: string; description: string };
+
 @Component({
   selector: 'noc-examples',
-  template: `I am examples`,
+  template: `
+    @for (example of examples; track $index) {
+      <h1>{{ example.header }}</h1>
+    } @empty {
+      <div class="flex items-center justify-center w-full h-full">
+        There are no examples yet. Come back in the future
+      </div>
+    }
+  `,
 })
-export class ExamplesComponent {}
+export class ExamplesComponent {
+  public readonly examples: Example[] = [];
+}
