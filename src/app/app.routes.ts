49dfc8889d3b50927f0b21c2b8c8cc7a05301c2d
Damian Kowalski <dkowalski.1997@gmail.com>
2026-08-23 16:50:31 +0200

    added route guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Examples } from './routes/examples/examples.component';
 import { ExamplePreview } from './routes/example-preview/example-preview.component';
+import { ExampleGuard } from './routes/example-preview/example-preview.guard';
 
 export const routes: Routes = [
   {
@@ -10,6 +11,7 @@ export const routes: Routes = [
   {
     path: 'example-preview/:id',
     component: ExamplePreview,
+    canActivate: [ExampleGuard],
   },
   {
     path: '**',
